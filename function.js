0dd08dfbe2b6223fb63e399f82a7584e361f3808
sheetal <135092130+sheetalmasih@users.noreply.github.com>
2024-04-12 14:48:58 +0530

    Add files via upload

---
+++ function.js
@@ -0,0 +1,95 @@
+// Given length and breadth of a rectangle, return the area of the rectangle.
+// const Areaofrectangle=(length,breadth)=>{
+//     return length*breadth;
+// }
+// let result=Areaofrectangle(20,20);
+// console.log(result);
+
+// Take a number and print the square of the number.
+
+// const squareofnumber=(num)=>{
+//     return num**2;
+// }
+// let square=squareofnumber(4);
+// console.log(square);
+
+// >Take two numbers and only print the integer part when a is divided by b.
+
+// const a_Divided_By_b=(a,b)=>{
+//     return Math.floor(a/b);
+// }
+// let integer_part=a_Divided_By_b(9,4);
+// console.log(integer_part);
+
+// Take a number and print out the last digit of the number.
+// const lastdigit=(Number)=>{
+//     return Number%10;
+
+// }
+// let last_digit=lastdigit(1234);
+// console.log(last_digit);
+
+// Take a number and find the cube of that number.
+// const Cubeofnumber=(number)=>{
+//     return number**3;
+// }
+// let result=Cubeofnumber(2);
+// console.log(result);
+
+// Take a 3 digit number and print the reverse of that number. Ex - 235 = 532
+//  const printrev=(num)=>{
+//     let rev=0
+//     let l;
+//     while(num>0){
+//         l=num%10
+//         rev=rev*10+l;
+//         num=Math.floor(num/10)
+//     }
+//     return rev
+//  }
+// let result=(printrev(-123))
+// console.log(result);
+
+// Take a number and print out the first digit of the number. Ex - 145 = 1, 789 = 7
+
+// const first_digit=(number)=>{
+//     return Math.floor(number/10)+number%10;
+// }
+// let first=first_digit(576);
+// console.log(first);
+
+// Take a number and return the sum of the first and last digit. Ex - 10 = 1+0 = 1, 452 = 4+2=7
+// const sumofFirst_and_last=(number)=>{
+//     let last_digit=number%10;
+//     let first_digit=0;
+//     while (number >= 10){
+//         number= Math.floor(number/10)
+//         first_digit = number
+//     }
+//     return first_digit+last_digit;
+// }
+// let result=sumofFirst_and_last(-432);
+// console.log(result);
+
+// Take salary of five employees and print the average salary of all the employees.
+// const Averagesalary=(employee1,employee2,employee3,employee4,employee5)=>{
+//     return (employee1+employee2+employee3+employee4+employee5)/5;
+// }
+// let result=Averagesalary(20000,10000,25000,15000,30000);
+// console.log(result);
+
+// Take two numbers and swap them. Ex - a = 10, b=5; =&gt; a=5, b=10;
+
+// const swap_the_numbers=(a,b)=>{
+//    let temp=0;
+//    temp=a;
+//    a=b;
+//    b=temp;
+//    return [a,b];
+// }
+// console.log(swap_the_numbers(6,3));
+
+
+
+
+
